13feef758d1389e0dd2a7fc8945ce7fbc58d38df
Manish Manohar <beingmanish.manohar@gmail.com>
2025-05-16 16:20:15 +0530

    Refactor Navbar and Hero components for UI improvements

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,13 @@
 const Hero = () => {
   return (
     <div className={"container-xl"}>
-      <section className="bg-indigo-500 py-20 mb-4">
+      <section className="bg-blue-500 py-20 mb-4">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
           <div className="text-center">
             <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
               Become a React Developer
             </h1>
             <p className="my-4 text-xl text-white">
-              {" "}
               Find the react job that fits your skill set.
             </p>
           </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,31 @@
+import logo from "../assets/react.svg";
+
 const Navbar = () => {
   return (
-    <nav>
-      <div className={"container-xl bg-indigo-500"}>
-        <div className={"text-center py-4"}>
-          <div className={"text-5xl font-extrabold"}>Navbar</div>
+    <nav className="bg-blue-700  border-b border-blue-500">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex h-20 items-center justify-between">
+          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+            <a className="flex flex-shrink-0 items-center mr-4">
+              <img className="h-10 w-auto" src={logo} alt="React Jobs" />
+              <span className="hidden md:block text-white text-2xl font-bold ml-2">
+                React Jobs
+              </span>
+            </a>
+            <div className="ml-auto">
+              <div className="flex space-x-2">
+                <a className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                  Home
+                </a>
+                <a className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                  Jobs
+                </a>
+                <a className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                  Add Job
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
