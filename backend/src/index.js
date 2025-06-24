0acdce2eed9e859f2316513aef4d50c8245e3d01
Manish Manohar <beingmanish.manohar@gmail.com>
2025-06-24 15:31:42 +0530

    Add job detail endpoint to backend API
    Introduce a new `/jobs/:id` endpoint to fetch job details by ID. Added error handling for non-existent or malformed job requests. Updated `jobs.json` data reading logic.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,16 +3,27 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
-
 const port = 3333;
-
+const dataPath = path.join(__dirname, 'jobs.json');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/jobs/:id', (req, res) => {
+    try {
+        const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+        const job = data.jobs.find(job => job.id.toString() === req.params.id.toString());
+        if (!job) {
+            res.status(404).json({error: "Job not found"});
+        }
+        res.json(job);
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+})
+
 app.get('/jobs', (req, res) => {
-    const dataPath = path.join(__dirname, 'jobs.json');
     try {
         const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
         res.json(data);
